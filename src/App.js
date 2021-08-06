8105262f6a58e5675799ead692e1e40e804a4207
Abhayks2808 <Abhayks2808@gmail.com>
2021-08-06 11:32:20 +0530

    profilecomponent and header added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import {Switch,Route,BrowserRouter as Router} from 'react-router-dom'
 import Feeds from './components/feeds';
-import Header from './components/header';
 import Login from './components/login';
 import './App.css';
 import PrivateRoute from './components/PrivateRoute';
 import { AuthProvider} from './context/Authcontext';
 import Signup from './components/signup';
+import Profile from './components/profile';
 
 function App() {
   return (
     <div className="App">
-       <Header></Header>
+       
       <AuthProvider>
       <Router>
+      
        <Switch>
          <Route path="/login" exact component={Login}></Route>
          <Route path="/signup" exact component={Signup}></Route>
+         <PrivateRoute path='/profile' component={Profile}></PrivateRoute>
          <PrivateRoute path="/"  component={Feeds}></PrivateRoute>
        </Switch>
       </Router>
