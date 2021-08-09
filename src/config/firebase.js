e6dad5aba8144433a6b8dc6f38beb03f40ec19c2
Abhayks2808 <61934562+Abhayks2808@users.noreply.github.com>
2021-08-09 20:57:49 +0530

    Delete firebase.js

--- src/config/firebase.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase"
-// put your firebase config file here
-
-
-  // Initialize Firebase
-let firebaseApp=firebase.initializeApp(firebaseconfig);
-export let firebaseAuth=firebaseApp.auth();
-export let firebasestoratege=firebaseApp.storage();
-export let firebasedb=firebaseApp.firestore();
